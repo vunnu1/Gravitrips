cdf616eab3a50046e09ce724b2bd53ac48ea8989
Daniil Skuridin <40205799+vunnu1@users.noreply.github.com>
2018-08-21 13:41:00 +0300

    Update Player.java

--- Gravitrips/src/Gravitrips/Player.java
+++ Gravitrips/src/Gravitrips/Player.java
@@ -1,21 +1,6 @@
 package Gravitrips;
 
 public abstract class Player {
-    private Token token;
-
-    public void setToken(Token token) {
-        this.token = token;
-    }
-
-    public boolean won = false;
-
-    public boolean isWon() {
-        return won;
-    }
-
-    public void setWon(boolean won) {
-        this.won = won;
-    }
 
     public abstract void move(Token[][] array);
 }
